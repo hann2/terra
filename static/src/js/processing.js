c5f04fabcc156aa51ff17292b2d53d54ee069191
Nick Jeffrey <njeffrey@indeed.com>
2015-07-30 18:04:34 -0500

    added prominent peaks to topographical map

--- static/src/js/processing.js
+++ static/src/js/processing.js
@@ -88,6 +88,100 @@ var processing = (function() {
         }
     };
 
+    /**
+     *  Return a list of local maxima
+     */
+    processing.getMax = function(data, width, height) {
+        var local = [];
+        for (var xpos = 0; xpos < width; xpos++){
+            for (var ypos = 0; ypos < height; ypos++){
+                var maxNeighbor = 0;
+                var myval = data[xpos + ypos * width];
+                for (var neighborX = -1; neighborX < 2; neighborX++){
+                    for (var neighborY = -1; neighborY < 2; neighborY++){
+                        var x = xpos + neighborX;
+                        var y = ypos + neighborY;
+                        if (x >= 0 && y >= 0 && x < width && y < height && data[x+y*width] >= myval){
+                            maxNeighbor = neighborX + 3 * neighborY;
+                            myval = data[x+y*width];
+                        }
+                    }
+                }
+                if (maxNeighbor === 0) {
+                    local.push(xpos + ypos * width);
+                }
+            }
+        }
+        return local;
+    }
+
+    /**
+     *  Return a list of prominence for each local maxima
+     *  Floodfill the points that can be reached by descending all local maxima
+     *  start at the smallest and working upwards
+     *  Then iterate through floodfilled values and record the lowest point for each maxima
+     *  
+     */
+    processing.getProminence = function(maxima, data, width, height) {
+        //order maxima
+        var orderedMaxima = maxima.slice();
+        orderedMaxima.sort(function(a, b){
+            return data[a] - data[b];
+        });
+        //floodfill
+        var floodfilled = data.slice();
+        for (var i = 0; i < orderedMaxima.length; i++){
+            processing.floodfillDescending(floodfilled, orderedMaxima[i], orderedMaxima[i], data, width, height);
+        }
+
+        //find prominence
+        var mapping = {};
+        for (var i = 0; i < orderedMaxima.length; i++){
+            mapping[orderedMaxima[i]] = maxima.indexOf(orderedMaxima[i]);
+        }
+        var prominence = [];
+        for (var i = 0; i < maxima.length; i++){
+            prominence.push(0);
+        }
+        for (var xpos = 0; xpos < width; xpos++){
+            for (var ypos = 0; ypos < height; ypos++){
+                var position = floodfilled[xpos + ypos * width]
+                var arrayPos = mapping[position];
+                var val = data[position] - data[xpos + ypos * width];
+                if (prominence[arrayPos] < val){
+                    prominence[arrayPos] = val;
+                }
+            }
+        }
+        return prominence;
+    }
+
+    /**
+     *  Prominence helper function
+     *  will wirte the value startingVal into the array floodfilled
+     *  if it can be reached by only descending heightmap values
+     *  starts at position startingVal
+     *  
+     */
+    processing.floodfillDescending = function(floodfilled, startingVal, writeVal, heightmap, width, height) {
+        floodfilled[startingVal] = writeVal;
+        var xpos = startingVal % width;
+        var ypos = Math.floor(startingVal / width);
+        var myVal = heightmap[startingVal];
+        for (var neighborx = -1; neighborx < 2; neighborx++){
+            for (var neighbory = -1; neighbory < 2; neighbory++){
+                var x = xpos + neighborx;
+                var y = ypos + neighbory;
+                var arrayPos = x + y*width;
+                if (x >= 0 && y >= 0 && x < width && y < height){
+                    if (floodfilled[arrayPos] != writeVal && heightmap[arrayPos] <= myVal){
+                        processing.floodfillDescending(floodfilled, arrayPos, writeVal, heightmap, width, height);
+                    }
+                }
+            }
+        }
+    }
+
     return processing;
 })();
 
